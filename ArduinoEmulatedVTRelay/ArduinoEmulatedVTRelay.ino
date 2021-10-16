d240416388dc9d72573e1997047afafa17563ba4
MatGyverTheCoder <mathewhasting@gmail.com>
2021-10-16 11:58:40 -0700

    v0.2

--- ArduinoEmulatedVTRelay/ArduinoEmulatedVTRelay.ino
+++ ArduinoEmulatedVTRelay/ArduinoEmulatedVTRelay.ino
@@ -5,7 +5,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-<<<<<<< HEAD
   
 }
 
@@ -16,7 +15,4 @@ bool fault() {
  //true, if fault detected
  //false, if fault is not detected
  return false;
-=======
-
->>>>>>> b3ffd6e663f54018fc819f573af73cee11b4aaf2
 }
